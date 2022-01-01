3fefbb204253c07ab32786d842295fb9a2a496ae
Nick Sullivan <nicholassull@gmail.com>
2022-01-01 14:41:11 -0800

    remove empty lines of code

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -5,23 +5,18 @@ describe('User', () => {
   beforeEach(() => {
     user = new User(26);
   });
-  
   test('Should create a new user with an age of 26', () => {
     expect(user.earthAge).toEqual(26);
   });
-
   test('Should return user age in Mercury years', () => {
     expect(user.mercuryAge()).toEqual(108);
   });
-
   test('Should return user age in Venus years', () => {
     expect(user.venusAge()).toEqual(41);
   });
-
   test('Should return user age in Mars years', () => {
     expect(user.marsAge()).toEqual(13);
   });
-
   test('Should return user age in Jupiter years', () => {
     expect(user.jupiterAge()).toEqual(2);
   });
--- src/js/user.js
+++ src/js/user.js
@@ -2,23 +2,19 @@ export default class User {
   constructor(age) {
     this.earthAge = age;
   }
-
   mercuryAge() {
     return Math.floor(this.earthAge / 0.24);
   }
-
   venusAge() {
     return Math.floor(this.earthAge / 0.62);
   }
-
   marsAge() {
     return Math.floor(this.earthAge / 1.88);
   }
-
   jupiterAge() {
     return Math.floor(this.earthAge / 11.86);
   }
-
+  
   yearsToLive(avgLife) {
     const yearsLeft = Math.floor(avgLife - this.earthAge);
     if (yearsLeft === 0) {
