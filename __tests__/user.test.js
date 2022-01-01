8e46aa778413e36fac1961e7970ba8e249037317
Nick Sullivan <nicholassull@gmail.com>
2022-01-01 12:20:45 -0800

    add beforeEach() function to user.test.js

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,8 +1,15 @@
 import User from './../src/js/user.js'
 
 describe('User', () => {
+  beforeEach(() => {
+    user = new User(26);
+  });
+  
   test('Should create a new user with an age of 26', () => {
-    let user = new User(26);
     expect(user.earthAge).toEqual(26);
+  });
+
+  test('Should return user age in Mercury years', () => {
+
   });
 });
